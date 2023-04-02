c6432332ee730af2ae3efc41b5e0fdc6cff54169
gagangani13 <127178242+gagangani13@users.noreply.github.com>
2023-04-02 23:40:18 +0530

    Lets start coding

---
+++ App.js
@@ -0,0 +1,10 @@
+import ExpenseItem from "./component/ExpenseItem";
+function App(){
+  return(
+    <div>
+      <h1>Expense Items</h1>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  )
+}
+export default App;

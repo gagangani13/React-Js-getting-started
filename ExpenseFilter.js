d82075d4e8535949c2e76075bb8d0452d031e446
gagangani13 <127178242+gagangani13@users.noreply.github.com>
2023-04-05 16:10:25 +0530

    Placeholder- Adding a filter which doesn't work

---
+++ ExpenseFilter.js
@@ -0,0 +1,17 @@
+const ExpenseFilter=(props)=>{
+    const choose=(e)=>{
+        props.onChangeOption(e.target.value)
+    }
+    return(
+        <div>
+            <label>Filter by year</label>
+            <select value={props.selected} onChange={choose}>
+                <option value="2023">2023</option>
+                <option value="2022">2022</option>
+                <option value="2021">2021</option>
+                <option value="2020">2020</option>
+            </select>
+        </div>
+    )
+}
+export default ExpenseFilter;
